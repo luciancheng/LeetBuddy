70b14132bf76cc499c7658bb23cb0ef38f4e4c54
Lucian Cheng <luciancheng3@gmail.com>
2025-01-12 08:16:11 -0500

    cleaning up code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -39,87 +39,8 @@ function App() {
           const targetRegex = /^(?:https?:\/\/)?leetcode\.com\/problems\/.*$/;
           const isTarget = targetRegex.test(tab.url);
           
+          // bool flag for checking we are on the correct page
           setIsTargetPage(isTarget);
-
-          // if (isTarget && tab.id) {
-          //   const [{ result }] = await chrome.scripting.executeScript({
-          //     target: { tabId: tab.id },
-          //     func: () => {
-          //       // Clone the body
-          //       const clone = document.body.cloneNode(true);
-          
-          //       // Check if an element is hidden from view
-          //       function isHiddenInLiveDOM(el) {
-          //         if (!el || el.nodeType !== Node.ELEMENT_NODE) return false;
-          //         const style = window.getComputedStyle(el);
-          //         if (
-          //           !style ||
-          //           style.display === 'none' ||
-          //           style.visibility === 'hidden' ||
-          //           parseFloat(style.opacity) === 0
-          //         ) {
-          //           return true;
-          //         }
-          //         return el.parentElement && isHiddenInLiveDOM(el.parentElement);
-          //       }
-          
-          //       // Remove any hidden elements
-          //       function removeHiddenElements(liveEl, cloneEl) {
-          //         const liveChildren = Array.from(liveEl.childNodes);
-          //         const cloneChildren = Array.from(cloneEl.childNodes);
-          
-          //         for (let i = 0; i < liveChildren.length; i++) {
-          //           const liveChild = liveChildren[i];
-          //           const cloneChild = cloneChildren[i];
-          //           if (!cloneChild) continue;
-          
-          //           if (liveChild.nodeType === Node.ELEMENT_NODE) {
-          //             if (isHiddenInLiveDOM(liveChild)) {
-          //               cloneChild.remove(); // remove from the clone
-          //             } else {
-          //               removeHiddenElements(liveChild, cloneChild);
-          //             }
-          //           }
-          //         }
-          //       }
-          //       removeHiddenElements(document.body, clone);
-          
-          //       // Replace <sup> in the clone with '^' + {text}
-          //       clone.querySelectorAll('sup').forEach(supEl => {
-          //         supEl.replaceWith('^{' + supEl.innerText + '}');
-          //       });
-
-          //       // Also replace <sub> in the clone with '_' + {text}
-          //       clone.querySelectorAll('sub').forEach(subEl => {
-          //         subEl.replaceWith('_{' + subEl.innerText + '}');
-          //       });
-          
-          //       // Create an offscreen container so the clone can be rendered (layout computed)
-          //       const offscreenContainer = document.createElement('div');
-          //       offscreenContainer.style.position = 'absolute';
-          //       offscreenContainer.style.left = '-999999px';
-          //       offscreenContainer.style.top = '0';
-          //       // (optional) set width to mimic the real body width if you want more accurate line breaks
-          //       // offscreenContainer.style.width = '1200px';
-          
-          //       // Add it to the DOM
-          //       document.documentElement.appendChild(offscreenContainer);
-          
-          //       // Move the clone inside the container
-          //       offscreenContainer.appendChild(clone);
-          
-          //       // Read the innerText from the now-rendered clone
-          //       const finalText = clone.innerText;
-          
-          //       // Remove the offscreen container
-          //       offscreenContainer.remove();
-          
-          //       // Return the final text to the extension
-          //       return finalText;
-          //     },
-          //   });
-          //   console.log(result);
-          // }
         }
       } catch (error) {
         console.error("Error fetching tab URL:", error);
